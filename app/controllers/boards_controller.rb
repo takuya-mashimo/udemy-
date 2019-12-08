90a02f8736ff624d2f3bb850dc80d8e1defea082
github takuya-mashimo <m.takku29@gmail.com>
2019-12-08 14:46:31 +0900

    問題一覧画面と問題投稿画面を作った

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,5 +1,21 @@
 # frozen_string_literal: true
 
 class BoardsController < ApplicationController
-  def index; end
+  def index
+    @boards = Board.all
+  end
+
+  def new
+    @board = Board.new
+  end
+
+  def create
+    Board.create(board_params)
+  end
+
+  private
+
+  def board_params
+    params.require(:board).permit(:major, :name, :title, :body)
+  end
 end
