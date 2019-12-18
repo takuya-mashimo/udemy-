89a2fb92806db5cd606d99a34c22bdc54292a06b
github takuya-mashimo <m.takku29@gmail.com>
2019-12-18 11:13:40 +0900

    ユーザー認証のためのbcryptを追加、controller,table等を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/me'
+
+  get 'home/index'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'
   resources :boards
   resources :comments, only: %i[create destroy]
 end
